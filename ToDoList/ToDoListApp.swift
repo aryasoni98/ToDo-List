4af01c16c615a3b8a5566c0eb12792bc731df066
aryasoni98 <aryasoni98@gmail.com>
2021-06-09 12:10:10 +0530

    first commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Arya Soni on 09/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
